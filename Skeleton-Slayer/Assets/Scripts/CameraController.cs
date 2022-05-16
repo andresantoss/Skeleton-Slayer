b417224d039b7d66d4b21660f9b49328a9bc1f83
André Santos <andreugabagos@sempreceub.com>
2022-05-16 08:03:49 -0300

    Stop Camera Flip & Inverting Camera

--- Skeleton-Slayer/Assets/Scripts/CameraController.cs
+++ Skeleton-Slayer/Assets/Scripts/CameraController.cs
@@ -9,6 +9,9 @@ public class CameraController : MonoBehaviour
     public bool useOffsetvalues;
     public float rotateSpeed;
     public Transform pivot;
+    public float maxViewAngle;
+    public float minViewAngle;
+    public bool invertY;
 
     // Start is called before the first frame update
     void Start()
@@ -32,11 +35,26 @@ public class CameraController : MonoBehaviour
         
         //Get the Y position of the mouse & rotate the pivot
         float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
-        pivot.Rotate(-vertical,0,0);
+        //pivot.Rotate(vertical,0,0);
+        if(invertY)
+        {
+            pivot.Rotate(vertical,0,0);
+        }
+        else
+        {  
+            pivot.Rotate(-vertical,0,0);
+        }
 
         //Limit up/down camera rotation
-        //if(pivot.rotation)
+        if(pivot.rotation.eulerAngles.x > maxViewAngle  && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle,0,0);
+        }
+        if(pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle,0,0);
 
+        }
 
         // Move the camera based on the current rotation of the target & the original offset
         float desiredYAngle = target.eulerAngles.y;
