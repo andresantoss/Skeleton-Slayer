78c68522384955b2e12150724c8bd3f4f185dc3d
André Santos <andreugabagos@sempreceub.com>
2022-05-16 07:36:57 -0300

    Moving With Camera Rotation

--- Skeleton-Slayer/Assets/Scripts/CameraController.cs
+++ Skeleton-Slayer/Assets/Scripts/CameraController.cs
@@ -8,6 +8,7 @@ public class CameraController : MonoBehaviour
     public Vector3 offset;
     public bool useOffsetvalues;
     public float rotateSpeed;
+    public Transform pivot;
 
     // Start is called before the first frame update
     void Start()
@@ -16,27 +17,39 @@ public class CameraController : MonoBehaviour
         {
             offset = target.position - transform.position;
         }
-        
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+        //Get the X position of the mouse & rotate the target
         float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
         target.Rotate(0, horizontal,0);
         
+        //Get the Y position of the mouse & rotate the pivot
         float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
-        target.Rotate(-vertical,0,0);
+        pivot.Rotate(-vertical,0,0);
+
+        //Limit up/down camera rotation
+        //if(pivot.rotation)
 
 
+        // Move the camera based on the current rotation of the target & the original offset
         float desiredYAngle = target.eulerAngles.y;
-        float desiredXAngle = target.eulerAngles.x;
-
+        float desiredXAngle = pivot.eulerAngles.x;
         Quaternion rotation = Quaternion.Euler(desiredXAngle,desiredYAngle,0);
         transform.position = target.position - (rotation * offset);
 
         //transform.position = target.position - offset;
-
+        
+        if(transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y -.5f, transform.position.z);
+        }
         transform.LookAt(target);
     }
 }
