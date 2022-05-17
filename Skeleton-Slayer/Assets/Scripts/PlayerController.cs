2b6c7b0efdfa2eb6eb80d69c43ccaf271754e34e
André Santos <andreugabagos@sempreceub.com>
2022-05-17 18:59:37 -0300

    Adding Player Model

--- Skeleton-Slayer/Assets/Scripts/PlayerController.cs
+++ Skeleton-Slayer/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float moveSpeed;
     //public Rigidbody theRB;
     public float jumpForce;
     public CharacterController controller;
+
+    public Animator anim;
 
     private Vector3 moveDirection;
     public float gravityScale;
@@ -38,14 +39,18 @@ public class PlayerController : MonoBehaviour
 
         if(controller.isGrounded)
         {
+            
             moveDirection.y = 0f;
             if(Input.GetButtonDown("Jump"))
-            {
+            {  
                 moveDirection.y = jumpForce;
             }
         }
         
         moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime); //gravidade
         controller.Move(moveDirection * Time.deltaTime);
+
+        anim.SetBool("isGrounded", controller.isGrounded);
+        anim.SetFloat("speed",(Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"))));
     }
 }
