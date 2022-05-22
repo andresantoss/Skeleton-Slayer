d50650ef81b6fb6e7d800e8fe239fa71c5b71991
André <andreugabagos@sempreceub.com>
2022-05-21 21:37:26 -0300

    Invencibility

--- Skeleton-Slayer/Assets/Scripts/HealthManager.cs
+++ Skeleton-Slayer/Assets/Scripts/HealthManager.cs
@@ -1,42 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
-    public int maxHealth;
-    public int currentHealth;
 
-    public PlayerController thePlayer;
+ MeshRenderer meshRenderer;
+ Color origColor;
+ // Health System
+ public int maxHealth;
+ public int currentHealth;
+ public PlayerController thePlayer;
+ public Animator anim;
+ // invicibility
+ public float invicibilityLength;
+ private float invicibilityCounter;
+ // invicibility effect
+ public GameObject bubble;
+ // Start is called before the first frame update
+ void Start()
+ {
+  currentHealth = maxHealth;
+  thePlayer = FindObjectOfType<PlayerController>();
+ }
+ // Update is called once per frame
+ void Update()
+ {
+  if (invicibilityCounter > 0)
+  {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentHealth = maxHealth;
+   invicibilityCounter -= Time.deltaTime;
+  }
+  else
+  {
+   bubble.SetActive(false);
+  }
+ }
+ public void HurtPlayer(int damage, Vector3 direction)
+ {
+  if (invicibilityCounter <= 0)
+  {
+   bubble.SetActive(true);
+   anim.SetInteger("hurt", 1);
+   currentHealth -= damage;
+   thePlayer.Knockback(direction);
+   invicibilityCounter = invicibilityLength;
+  }
+ }
+ public void HealPlayer(int healAmount)
+ {
+  currentHealth += healAmount;
+  if (currentHealth > maxHealth)
+  {
+   currentHealth = maxHealth;
+  }
+ }
 
-        thePlayer = FindObjectOfType<PlayerController>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    public void HurtPlayer(int damage, Vector3 direction)
-    {
-        currentHealth -= damage;
-
-        thePlayer.Knockback(direction);
-    }
-
-    public void HealPlayer(int healAmount)
-    {
-        currentHealth += healAmount;
-
-        if(currentHealth > maxHealth)
-        {
-            currentHealth = maxHealth;
-        }
-    }
 }
--- Skeleton-Slayer/Assets/Scripts/PlayerController.cs
+++ Skeleton-Slayer/Assets/Scripts/PlayerController.cs
@@ -1,23 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
  public float moveSpeed;
-
  public float jumpForce;
  public CharacterController controller;
-
  private Vector3 moveDirection;
  public float gravityScale;
-
- public Animator anim; 
+ public Animator anim;
  public Transform pivot;
  public float rotateSpeeed;
-
  public GameObject playerModel;
-
  public float knockBackForce;
  public float knockBackTime;
  private float knockBackCounter;
@@ -30,47 +22,46 @@ public class PlayerController : MonoBehaviour
 
  // Update is called once per frame
  void Update()
+ {
+  if (knockBackCounter <= 0)
+  {
+   float yStore = moveDirection.y;
+   moveDirection = ((transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal")));
+   moveDirection = moveDirection.normalized * moveSpeed;
+   moveDirection.y = yStore;
+
+   if (controller.isGrounded)
+   {
+    moveDirection.y = 0f;
+    if (Input.GetButtonDown("Jump"))
     {
-        if(knockBackCounter <=0)
-        {
-            float yStore = moveDirection.y;
-            moveDirection = ((transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal")));
-            moveDirection = moveDirection.normalized * moveSpeed;
-            moveDirection.y = yStore;
-
-            if (controller.isGrounded)
-            {
-                moveDirection.y = 0f;
-                if (Input.GetButtonDown("Jump"))
-                {
-                    moveDirection.y = jumpForce;
-                }
-            }
-        } else
-        {
-            knockBackCounter -= Time.deltaTime;
-        }
-    
-        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime); //gravidade
-        controller.Move(moveDirection * Time.deltaTime);
-
-        //Move player different directions based on camera look
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-        {
-        transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
-        Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
-        playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeeed * Time.deltaTime);
-        }
-        anim.SetFloat ("speed", Mathf.Abs (Input.GetAxis ("Horizontal")));
-        anim.SetFloat ("speed", Mathf.Abs (Input.GetAxis ("Vertical")));
-        anim.SetBool("isGrounded", controller.isGrounded);
+     moveDirection.y = jumpForce;
     }
+   }
+  }
+  else
+  {
+   knockBackCounter -= Time.deltaTime;
+  }
+  moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime); //gravidade
+  controller.Move(moveDirection * Time.deltaTime);
+  //Move player different directions based on camera look
+  if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+  {
+   transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+   Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+   playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeeed * Time.deltaTime);
+  }
+  anim.SetFloat("speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+  anim.SetFloat("speed", Mathf.Abs(Input.GetAxis("Vertical")));
+  anim.SetBool("isGrounded", controller.isGrounded);
+ }
 
-    public void Knockback(Vector3 direction)
-    {
-        knockBackCounter = knockBackTime;
+ public void Knockback(Vector3 direction)
+ {
+  knockBackCounter = knockBackTime;
 
-        moveDirection = direction * knockBackForce;
-        moveDirection.y = knockBackForce;
-    }
+  moveDirection = direction * knockBackForce;
+  moveDirection.y = knockBackForce;
+ }
 }
