6ba4ad4bbf0f0c56e1efd0ca724f35f7b6697e7f
André <andreugabagos@sempreceub.com>
2022-06-03 09:50:26 -0300

    animation AttackBasic (FIXED)

--- Skeleton-Slayer/Assets/Scripts/PlayerController.cs
+++ Skeleton-Slayer/Assets/Scripts/PlayerController.cs
@@ -14,68 +14,75 @@ public class PlayerController : MonoBehaviour
     public float knockBackForce;
     public float knockBackTime;
     private float knockBackCounter;
-
     // respawn
     public Transform respawnPoint;
-
     public GameObject mainMenu;
     public GameObject youDie;
     public GameObject hud;
     public GameObject cpWarning;
-
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
     }
-
-    // Update is called once per frame
     [System.Obsolete]
     void Update()
     {
         if (mainMenu.active == false)
         {
-
-
             if (anim.GetInteger("transition") != 4)
             {
+                Debug.Log(knockBackCounter);
                 if (knockBackCounter <= 0)
                 {
-                    float yStore = moveDirection.y;
-                    float zDirection = Input.GetAxis("Vertical");
-                    moveDirection = new Vector3(0.0f, 0.0f, zDirection);
-                    moveDirection = moveDirection.normalized * moveSpeed; // normaliza a velocidade
-                    moveDirection.y = yStore;
-                    moveDirection = transform.TransformDirection(moveDirection);
-                    rot += Input.GetAxis("Horizontal") * rotateSpeeed * Time.deltaTime;
-                    transform.eulerAngles = new Vector3(0f, rot, 0f);
-                    if (controller.isGrounded)
+                    if (Input.GetAxis("Fire1") != 0)
                     {
-                        moveDirection.y = 0f;
-                        transform.position += transform.forward * moveSpeed;
-                        if (Input.GetButtonDown("Jump"))
+                        int xcount = Random.Range(2, 4);
+                        //Debug.Log(xcount);
+                        anim.SetInteger("transition", xcount);
+                        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime); //gravidade
+                        if (controller.isGrounded == false)
                         {
-                            moveDirection.y = jumpForce;
+                            controller.Move(moveDirection * Time.deltaTime);
                         }
+                    }
+                    else
+                    {
+                        float yStore = moveDirection.y;
+                        float zDirection = Input.GetAxis("Vertical");
+                        moveDirection = new Vector3(0.0f, 0.0f, zDirection);
+                        moveDirection = moveDirection.normalized * moveSpeed; // normaliza a velocidade
+                        moveDirection.y = yStore;
+                        moveDirection = transform.TransformDirection(moveDirection);
+                        rot += Input.GetAxis("Horizontal") * rotateSpeeed * Time.deltaTime;
+                        transform.eulerAngles = new Vector3(0f, rot, 0f);
+                        if (controller.isGrounded)
+                        {
+                            moveDirection.y = 0f;
+                            transform.position += transform.forward * moveSpeed;
+                            if (Input.GetButtonDown("Jump"))
+                            {
+                                moveDirection.y = jumpForce;
+                            }
+                        }
+                        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime); //gravidade
+                        controller.Move(moveDirection * Time.deltaTime);
+                        //animação
+                        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+                        {
+                            anim.SetInteger("transition", 1);
+                        }
+                        else
+                        {
+                            anim.SetInteger("transition", 0);
+                        }
+                        anim.SetBool("isGrounded", controller.isGrounded);
                     }
                 }
                 else
                 {
                     knockBackCounter -= Time.deltaTime;
                 }
-                moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime); //gravidade
-                controller.Move(moveDirection * Time.deltaTime);
-
-                //animação
-                if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-                {
-                    anim.SetInteger("transition", 1);
-                }
-                else
-                {
-                    anim.SetInteger("transition", 0);
-                }
-                anim.SetBool("isGrounded", controller.isGrounded);
             }
             else
             {
@@ -84,7 +91,6 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
-
     public void SetSpawnPoint(Transform newPosition)
     {
         if (newPosition != respawnPoint)
@@ -93,7 +99,6 @@ public class PlayerController : MonoBehaviour
             respawnPoint = newPosition;
         }
     }
-
     public IEnumerator resetRespawn()
     {
         youDie.SetActive(true);
@@ -106,18 +111,15 @@ public class PlayerController : MonoBehaviour
         anim.SetInteger("transition", 0);
         FindObjectOfType<HealthManager>().HealPlayer(9999);
     }
-
     public IEnumerator checkpointWarning()
     {
         cpWarning.SetActive(true);
         yield return new WaitForSeconds(1f);
         cpWarning.SetActive(false);
     }
-
     public void Knockback(Vector3 direction)
     {
         knockBackCounter = knockBackTime;
-
         moveDirection = direction * knockBackForce;
         moveDirection.y = knockBackForce;
     }
